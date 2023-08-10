b4629fdd58344ab6fd8d9824809e8d5cad9bc9b9
Leanora Polera <141896841+leanora-polera@users.noreply.github.com>
2023-08-10 05:02:41 +0000

    Enable crushing and smelting-1 for all ores
    Enable crushing and smelting-1 for all ores.  smelting-2 is only enabled
    for those with the needed requirements.

--- prototypes/recipes/ore-recipes.lua
+++ prototypes/recipes/ore-recipes.lua
@@ -1,42 +1,82 @@
-enabledOres = {}
-if data.raw["recipe"]["akaganeite-smelting-2"]      then table.insert(enabledOres, {input = "akaganeite", output = "iron"}) end
-if data.raw["recipe"]["ankerite-smelting-2"]        then table.insert(enabledOres, {input = "ankerite", output = "iron"}) end
-if data.raw["recipe"]["cronstedtite-smelting-2"]    then table.insert(enabledOres, {input = "cronstedtite", output = "iron"}) end
-if data.raw["recipe"]["ferrihydrite-smelting-2"]    then table.insert(enabledOres, {input = "ferrihydrite", output = "iron"}) end
-if data.raw["recipe"]["goethite-smelting-2"]        then table.insert(enabledOres, {input = "goethite", output = "iron"}) end
-if data.raw["recipe"]["greenalite-smelting-2"]      then table.insert(enabledOres, {input = "greenalite", output = "iron"}) end
-if data.raw["recipe"]["greigite-smelting-2"]        then table.insert(enabledOres, {input = "greigite", output = "iron"}) end
-if data.raw["recipe"]["grunerite-smelting-2"]       then table.insert(enabledOres, {input = "grunerite", output = "iron"}) end
-if data.raw["recipe"]["hematite-smelting-2"]        then table.insert(enabledOres, {input = "hematite", output = "iron"}) end
-if data.raw["recipe"]["lepidocrocite-smelting-2"]   then table.insert(enabledOres, {input = "lepidocrocite", output = "iron"}) end
-if data.raw["recipe"]["macaulayite-smelting-2"]     then table.insert(enabledOres, {input = "macaulayite", output = "iron"}) end
-if data.raw["recipe"]["magnesioferrite-smelting-2"] then table.insert(enabledOres, {input = "magnesioferrite", output = "iron"}) end
-if data.raw["recipe"]["magnetite-smelting-2"]       then table.insert(enabledOres, {input = "magnetite", output = "iron"}) end
-if data.raw["recipe"]["minnesotaite-smelting-2"]    then table.insert(enabledOres, {input = "minnesotaite", output = "iron"}) end
-if data.raw["recipe"]["pyrite-smelting-2"]          then table.insert(enabledOres, {input = "pyrite", output = "iron"}) end
-if data.raw["recipe"]["pyrrhotite-smelting-2"]      then table.insert(enabledOres, {input = "pyrrhotite", output = "iron"}) end
-if data.raw["recipe"]["roaldite-smelting-2"]        then table.insert(enabledOres, {input = "roaldite", output = "iron"}) end
-if data.raw["recipe"]["schwertmannite-smelting-2"]  then table.insert(enabledOres, {input = "schwertmannite", output = "iron"}) end
-if data.raw["recipe"]["siderite-smelting-2"]        then table.insert(enabledOres, {input = "siderite", output = "iron"}) end
-if data.raw["recipe"]["stilpnomelane-smelting-2"]   then table.insert(enabledOres, {input = "stilpnomelane", output = "iron"}) end
-if data.raw["recipe"]["taenite-smelting-2"]         then table.insert(enabledOres, {input = "taenite", output = "iron"}) end
-if data.raw["recipe"]["trevorite-smelting-2"]       then table.insert(enabledOres, {input = "trevorite", output = "iron"}) end
-if data.raw["recipe"]["troilite-smelting-2"]        then table.insert(enabledOres, {input = "troilite", output = "iron"}) end
-if data.raw["recipe"]["wustite-smelting-2"]         then table.insert(enabledOres, {input = "wustite", output = "iron"}) end
-if data.raw["recipe"]["azurite-smelting-2"]         then table.insert(enabledOres, {input = "azurite", output = "copper"}) end
-if data.raw["recipe"]["bornite-smelting-2"]         then table.insert(enabledOres, {input = "bornite", output = "copper"}) end
-if data.raw["recipe"]["chalcocite-smelting-2"]      then table.insert(enabledOres, {input = "chalcocite", output = "copper"}) end
-if data.raw["recipe"]["chalcopyrite-smelting-2"]    then table.insert(enabledOres, {input = "chalcopyrite", output = "copper"}) end
-if data.raw["recipe"]["chrysocolla-smelting-2"]     then table.insert(enabledOres, {input = "chrysocolla", output = "copper"}) end
-if data.raw["recipe"]["covellite-smelting-2"]       then table.insert(enabledOres, {input = "covellite", output = "copper"}) end
-if data.raw["recipe"]["cuprite-smelting-2"]         then table.insert(enabledOres, {input = "cuprite", output = "copper"}) end
-if data.raw["recipe"]["dioptase-smelting-2"]        then table.insert(enabledOres, {input = "dioptase", output = "copper"}) end
-if data.raw["recipe"]["djurleite-smelting-2"]       then table.insert(enabledOres, {input = "djurleite", output = "copper"}) end
-if data.raw["recipe"]["enargite-smelting-2"]        then table.insert(enabledOres, {input = "enargite", output = "copper"}) end
-if data.raw["recipe"]["malachite-smelting-2"]       then table.insert(enabledOres, {input = "malachite", output = "copper"}) end
-if data.raw["recipe"]["tennantite-smelting-2"]      then table.insert(enabledOres, {input = "tennantite", output = "copper"}) end
-if data.raw["recipe"]["tenorite-smelting-2"]        then table.insert(enabledOres, {input = "tenorite", output = "copper"}) end
-if data.raw["recipe"]["tetrahedite-smelting-2"]     then table.insert(enabledOres, {input = "tetrahedite", output = "copper"}) end
+enabledOres = {
+  akaganeite = {input = "akaganeite", output = "iron", smelting2 = false},
+  ankerite = {input = "ankerite", output = "iron", smelting2 = false},
+  cronstedtite = {input = "cronstedtite", output = "iron", smelting2 = false},
+  ferrihydrite = {input = "ferrihydrite", output = "iron", smelting2 = false},
+  goethite = {input = "goethite", output = "iron", smelting2 = false},
+  greenalite = {input = "greenalite", output = "iron", smelting2 = false},
+  greigite = {input = "greigite", output = "iron", smelting2 = false},
+  grunerite = {input = "grunerite", output = "iron", smelting2 = false},
+  hematite = {input = "hematite", output = "iron", smelting2 = false},
+  lepidocrocite = {input = "lepidocrocite", output = "iron", smelting2 = false},
+  macaulayite = {input = "macaulayite", output = "iron", smelting2 = false},
+  magnesioferrite = {input = "magnesioferrite", output = "iron", smelting2 = false},
+  magnetite = {input = "magnetite", output = "iron", smelting2 = false},
+  minnesotaite = {input = "minnesotaite", output = "iron", smelting2 = false},
+  pyrite = {input = "pyrite", output = "iron", smelting2 = false},
+  pyrrhotite = {input = "pyrrhotite", output = "iron", smelting2 = false},
+  roaldite = {input = "roaldite", output = "iron", smelting2 = false},
+  schwertmannite = {input = "schwertmannite", output = "iron", smelting2 = false},
+  siderite = {input = "siderite", output = "iron", smelting2 = false},
+  stilpnomelane = {input = "stilpnomelane", output = "iron", smelting2 = false},
+  taenite = {input = "taenite", output = "iron", smelting2 = false},
+  trevorite = {input = "trevorite", output = "iron", smelting2 = false},
+  troilite = {input = "troilite", output = "iron", smelting2 = false},
+  wustite = {input = "wustite", output = "iron", smelting2 = false},
+  azurite = {input = "azurite", output = "copper", smelting2 = false},
+  bornite = {input = "bornite", output = "copper", smelting2 = false},
+  chalcocite = {input = "chalcocite", output = "copper", smelting2 = false},
+  chalcopyrite = {input = "chalcopyrite", output = "copper", smelting2 = false},
+  chrysocolla = {input = "chrysocolla", output = "copper", smelting2 = false},
+  covellite = {input = "covellite", output = "copper", smelting2 = false},
+  cuprite = {input = "cuprite", output = "copper", smelting2 = false},
+  dioptase = {input = "dioptase", output = "copper", smelting2 = false},
+  djurleite = {input = "djurleite", output = "copper", smelting2 = false},
+  enargite = {input = "enargite", output = "copper", smelting2 = false},
+  malachite = {input = "malachite", output = "copper", smelting2 = false},
+  tennantite = {input = "tennantite", output = "copper", smelting2 = false},
+  tenorite = {input = "tenorite", output = "copper", smelting2 = false},
+  tetrahedite = {input = "tetrahedite", output = "copper", smelting2 = false},
+}
+
+if data.raw["recipe"]["akaganeite-smelting-2"]      then enabledOres.akaganeite.smelting2 = true end
+if data.raw["recipe"]["ankerite-smelting-2"]        then enabledOres.ankerite.smelting2 = true end
+if data.raw["recipe"]["cronstedtite-smelting-2"]    then enabledOres.cronstedtite.smelting2 = true end
+if data.raw["recipe"]["ferrihydrite-smelting-2"]    then enabledOres.ferrihydrite.smelting2 = true end
+if data.raw["recipe"]["goethite-smelting-2"]        then enabledOres.goethite.smelting2 = true end
+if data.raw["recipe"]["greenalite-smelting-2"]      then enabledOres.greenalite.smelting2 = true end
+if data.raw["recipe"]["greigite-smelting-2"]        then enabledOres.greigite.smelting2 = true end
+if data.raw["recipe"]["grunerite-smelting-2"]       then enabledOres.grunerite.smelting2 = true end
+if data.raw["recipe"]["hematite-smelting-2"]        then enabledOres.hematite.smelting2 = true end
+if data.raw["recipe"]["lepidocrocite-smelting-2"]   then enabledOres.lepidocrocite.smelting2 = true end
+if data.raw["recipe"]["macaulayite-smelting-2"]     then enabledOres.macaulayite.smelting2 = true end
+if data.raw["recipe"]["magnesioferrite-smelting-2"] then enabledOres.magnesioferrite.smelting2 = true end
+if data.raw["recipe"]["magnetite-smelting-2"]       then enabledOres.magnetite.smelting2 = true end
+if data.raw["recipe"]["minnesotaite-smelting-2"]    then enabledOres.minnesotaite.smelting2 = true end
+if data.raw["recipe"]["pyrite-smelting-2"]          then enabledOres.pyrite.smelting2 = true end
+if data.raw["recipe"]["pyrrhotite-smelting-2"]      then enabledOres.pyrrhotite.smelting2 = true end
+if data.raw["recipe"]["roaldite-smelting-2"]        then enabledOres.roaldite.smelting2 = true end
+if data.raw["recipe"]["schwertmannite-smelting-2"]  then enabledOres.schwertmannite.smelting2 = true end
+if data.raw["recipe"]["siderite-smelting-2"]        then enabledOres.siderite.smelting2 = true end
+if data.raw["recipe"]["stilpnomelane-smelting-2"]   then enabledOres.stilpnomelane.smelting2 = true end
+if data.raw["recipe"]["taenite-smelting-2"]         then enabledOres.taenite.smelting2 = true end
+if data.raw["recipe"]["trevorite-smelting-2"]       then enabledOres.trevorite.smelting2 = true end
+if data.raw["recipe"]["troilite-smelting-2"]        then enabledOres.troilite.smelting2 = true end
+if data.raw["recipe"]["wustite-smelting-2"]         then enabledOres.wustite.smelting2 = true end
+if data.raw["recipe"]["azurite-smelting-2"]         then enabledOres.azurite.smelting2 = true end
+if data.raw["recipe"]["bornite-smelting-2"]         then enabledOres.bornite.smelting2 = true end
+if data.raw["recipe"]["chalcocite-smelting-2"]      then enabledOres.chalcocite.smelting2 = true end
+if data.raw["recipe"]["chalcopyrite-smelting-2"]    then enabledOres.chalcopyrite.smelting2 = true end
+if data.raw["recipe"]["chrysocolla-smelting-2"]     then enabledOres.chrysocolla.smelting2 = true end
+if data.raw["recipe"]["covellite-smelting-2"]       then enabledOres.covellite.smelting2 = true end
+if data.raw["recipe"]["cuprite-smelting-2"]         then enabledOres.cuprite.smelting2 = true end
+if data.raw["recipe"]["dioptase-smelting-2"]        then enabledOres.dioptase.smelting2 = true end
+if data.raw["recipe"]["djurleite-smelting-2"]       then enabledOres.djurleite.smelting2 = true end
+if data.raw["recipe"]["enargite-smelting-2"]        then enabledOres.enargite.smelting2 = true end
+if data.raw["recipe"]["malachite-smelting-2"]       then enabledOres.malachite.smelting2 = true end
+if data.raw["recipe"]["tennantite-smelting-2"]      then enabledOres.tennantite.smelting2 = true end
+if data.raw["recipe"]["tenorite-smelting-2"]        then enabledOres.tenorite.smelting2 = true end
+if data.raw["recipe"]["tetrahedite-smelting-2"]     then enabledOres.tetrahedite.smelting2 = true end
 
 local prerequisitesop1 = {}
 if mods.Krastorio2 then
@@ -71,11 +111,13 @@ data:extend({
   prerequisites = {"oil-processing", "ore-processing-1"},
 }
 })
-for _, enabledOre in ipairs(enabledOres) do
-  table.insert(data.raw["technology"]["ore-processing-2"].effects, {type = "unlock-recipe", recipe = enabledOre.input .. "-smelting-2"})
+for _, enabledOre in pairs(enabledOres) do
+  if enabledOre.smelting2 then
+    table.insert(data.raw["technology"]["ore-processing-2"].effects, {type = "unlock-recipe", recipe = enabledOre.input .. "-smelting-2"})
+  end
 end
 
-for _, enabledOre in ipairs(enabledOres) do
+for _, enabledOre in pairs(enabledOres) do
   local iconPathOre = "__base__/graphics/icons/" .. enabledOre.output .. "-ore.png"
   local iconPathPlate = "__base__/graphics/icons/" .. enabledOre.output .. "-plate.png"
   data:extend({
