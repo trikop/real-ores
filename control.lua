bef9e92a60fb4093181e8e456db3280ce574fa57
trikop <trikopgaming@gmail.com>
2025-09-24 14:47:10 +0200

    options are many

--- control.lua
+++ control.lua
@@ -1,7 +1,7 @@
-script.on_init(function(event)
-    if remote.interfaces["freeplay"] then
-        local items = remote.call("freeplay", "get_ship_items")
-        items["iron-plate"] = 8
-        remote.call("freeplay", "set_ship_items", items)
-    end
-end)
+-- script.on_init(function(event)
+--     if remote.interfaces["freeplay"] then
+--         local items = remote.call("freeplay", "get_ship_items")
+--         items["iron-plate"] = 8
+--         remote.call("freeplay", "set_ship_items", items)
+--     end
+-- end)
--- data-updates.lua
+++ data-updates.lua
@@ -1,3 +1,3 @@
-require("prototypes/changes/mining-drill")
-require("prototypes/recipes/ore-recipes")
-require("prototypes/changes/ores")
+-- require("prototypes/changes/mining-drill")
+-- require("prototypes/recipes/ore-recipes")
+-- require("prototypes/changes/ores")
