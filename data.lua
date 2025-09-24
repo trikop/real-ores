1a981f5c1857d1217c049b861afa24a5b9f7b7f6
trikop <trikopgaming@gmail.com>
2025-09-24 23:47:38 +0200

    hmm

--- data.lua
+++ data.lua
@@ -3,3 +3,106 @@ require("prototypes/ores/define-ores")
 -- -- if not mods.Krastorio2 then
 --     require("prototypes/buildings/ore-crusher")
 -- -- end
+
+-- --require("prototypes/ores/default")
+
+-- -- Iron main
+-- require("prototypes/ores/iron/hematite")
+-- require("prototypes/ores/iron/magnetite")
+-- require("prototypes/ores/iron/siderite")
+-- require("prototypes/ores/iron/goethite")
+-- require("prototypes/ores/iron/wustite")
+-- require("prototypes/ores/iron/ferrihydrite")
+-- require("prototypes/ores/iron/lepidocrocite")
+-- require("prototypes/ores/iron/schwertmannite")
+-- require("prototypes/ores/iron/cronstedtite")
+
+-- --Iron/sulfur ores
+-- require("prototypes/ores/iron/pyrite")
+-- require("prototypes/ores/iron/troilite")
+-- require("prototypes/ores/iron/pyrrhotite")
+-- require("prototypes/ores/iron/greigite")
+
+-- --Copper ores
+-- require("prototypes/ores/copper/chalcopyrite")
+-- require("prototypes/ores/copper/chalcocite")
+-- require("prototypes/ores/copper/covellite")
+-- require("prototypes/ores/copper/djurleite")
+-- require("prototypes/ores/copper/bornite")
+-- require("prototypes/ores/copper/malachite")
+-- require("prototypes/ores/copper/azurite")
+-- require("prototypes/ores/copper/cuprite")
+-- require("prototypes/ores/copper/tenorite")
+
+-- --Coal ores
+-- require("prototypes/ores/coal/dinite")
+-- require("prototypes/ores/coal/refikite")
+-- require("prototypes/ores/coal/humboldtine")
+
+-- --Uranium ores
+-- require("prototypes/ores/uranium/uraninite")
+
+
+-- require("prototypes/ores/iron/greenalite")
+-- require("prototypes/ores/iron/grunerite")
+
+
+-- require("prototypes/ores/iron/macaulayite")
+-- require("prototypes/ores/iron/stilpnomelane")
+
+
+
+-- require("prototypes/ores/copper/dioptase")
+-- require("prototypes/ores/uranium/coffinite")
+
+
+-- require("prototypes/ores/copper/chrysocolla")
+
+
+
+-- require("prototypes/ores/iron/roaldite")
+-- require("prototypes/ores/iron/taenite")
+
+-- require("prototypes/ores/iron/akaganeite")
+
+-- require("prototypes/ores/iron/trevorite")
+-- require("prototypes/ores/iron/magnesioferrite")
+
+-- require("prototypes/ores/iron/ankerite")
+
+
+-- require("prototypes/ores/iron/minnesotaite")
+
+
+
+-- require("prototypes/ores/copper/enargite")
+-- require("prototypes/ores/copper/tennantite")
+-- require("prototypes/ores/copper/tetrahedite")
+
+
+
+-- -- require("prototypes/ores/coal/wattevilleite")
+
+-- require("prototypes/ores/coal/niveolanite")
+
+
+-- require("prototypes/ores/coal/eitelite")
+
+
+
+
+-- require("prototypes/ores/coal/lansfordite")
+
+
+
+
+-- require("prototypes/ores/coal/kochsandorite")
+
+
+-- require("prototypes/ores/uranium/brannerite")
+
+
+-- require("prototypes/ores/uranium/davidite-ce")
+
+-- require("prototypes/ores/uranium/davidite-la")
+
