a5cfd9194672468af4ab76c7b225f7df8124b064
trikop <trikopgaming@gmail.com>
2025-09-30 22:44:27 +0200

    Update define-ores.lua

--- prototypes/ores/define-ores.lua
+++ prototypes/ores/define-ores.lua
@@ -1,52 +1,63 @@
 local resource_autoplace = require("resource-autoplace");
 local item_sounds = require('__base__.prototypes.item_sounds')
 
+local function is_enabled(name)
+  for _, n in ipairs(name) do
+    if data.raw.item[n] then
+      return n
+    end
+  end
+  return nil
+end
+
 local enabled = {
   iron_ore = "iron-ore",
   copper_ore = "copper-ore",
-  coal = "coal",
+  carbon = "coal",
   uranium_ore = "uranium-ore",
 }
 
-if data.raw.item["magnesium-ore"] then
-  local ore = {magnesium_ore = "magnesium-ore"}
-  table.insert(enabled, ore)
-end
+enabled.lead = is_enabled({"lead", "pm-lead"}) -- Maybe take inspo from PM's impure ore mechanic
+enabled.sodium = is_enabled({"sodium", "pm-sodium"})
+enabled.tin = is_enabled({"tin-ore", "pm-tin-ore"})
+enabled.magnesium = is_enabled({"magnesium-ore", "pm-magnesium-ore"})
 
-
-
--- if data.raw.item["sodium"] then
---   existing_ores_from_mods["sodium"] = true
--- end
+enabled.hydrogen = is_enabled({"hydrogen", "kr-hydrogen"})
+enabled.oxygen = is_enabled({"oxygen", "kr-oxygen"})
 
 local function make_ore(ore)
-  local icon, filename, mining_particle, mapcolor
-  if ore.icon and ore.filename and ore.mining_particle and ore.map_color then
+  local icon, filename, mining_particle, mapcolor, ore_type
+  if ore.icon and ore.filename and ore.mining_particle and ore.map_color and ore.ore_type then
     icon = ore.icon
     filename = ore.filename
     mining_particle = ore.mining_particle
     mapcolor = ore.map_color
+    ore_type = ore.ore_type
   else
     if ore.type == "coal" then
       icon = "__base__/graphics/icons/coal"
       filename = "__base__/graphics/entity/coal/coal"
       mining_particle = "coal-particle"
       mapcolor = {0,0,0}
+      ore_type = "coal"
     elseif ore.type == "copper" then
       icon = "__base__/graphics/icons/copper-ore"
       filename = "__base__/graphics/entity/copper-ore/copper-ore"
       mining_particle = "copper-ore-particle"
       mapcolor = {0.803,0.388,0.215}
+      ore_type = "copper-ore"
     elseif ore.type == "iron" then
       icon = "__base__/graphics/icons/iron-ore"
       filename = "__base__/graphics/entity/iron-ore/iron-ore"
       mining_particle = "iron-ore-particle"
       mapcolor = {0.415,0.525,0.580}
+      ore_type = "iron-ore"
     elseif ore.type == "uranium" then
       icon = "__base__/graphics/icons/uranium-ore"
       filename = "__base__/graphics/entity/uranium-ore/uranium-ore"
       mining_particle = nil
       mapcolor = {0,0.7,0}
+      ore_type = "uranium-ore"
     else
       icon = "__base__/graphics/icons/coal"
       filename = "__base__/graphics/entity/coal/coal"
@@ -58,6 +69,7 @@ local function make_ore(ore)
     name = ore.name,
     results = ore.results or {{type="item", name=ore.name, amount_min=1, amount_max=1}},
     type = ore.type,
+    ore_type = ore_type or nil,
     fuel_value = ore.fuel_value or nil,
     weight = ore.weight or 4.5*kg,
     hardness = ore.hardness or 1,
@@ -74,29 +86,62 @@ local function make_ore(ore)
     filename = filename,
     mining_particle = mining_particle,
     processing_results = ore.processing_results or nil,
+
   }
 end
 
 local define_ores = {
   {
     name = "dinite",
-    type = "coal",   
-    fuel_value = "5MJ",   map_color = {0,0,0}, start_placement = true,
+    type = "coal",
+    fuel_value = "5MJ",
+    start_placement = true,
     processing_results = {
-      {type="item", name="coal", amount = 1, probability=.8688},
+      data.raw.item[enabled.carbon] and {type="item", name=enabled.carbon, amount = 1, probability=.8688},
       data.raw.fluid["hydrogen"] and {type="fluid", name="hydrogen", amount = 1, probability=.1312} or
       {type="fluid", name="water", amount = 1, probability=.1312},
     }
   },
-  --{ name = "eitelite",        type = "coal",   fuel_value = "1MJ",   , start_placement = true, processing_results = {{type="item", name="sodium", amount=1, probability=.2416}, {type="item", name="magnesium-ore", amount=1, probability=.1277}, {type="item", name="coal", amount=1, probability=.1262}, {type="fluid", name="oxygen", amount=1, probability=.5044}}},
-  -- {name = "humboldtine",     type = "coal",   fuel_value = "1MJ",   , start_placement = true},
-  -- {name = "kochsandorite",   type = "coal",   fuel_value = "0.5MJ", },
-  -- {name = "lansfordite",     type = "coal",   fuel_value = "0.5MJ", },
-  -- {name = "niveolanite",     type = "coal",   fuel_value = "0.5MJ", },
-  -- {name = "refikite",        type = "coal",   fuel_value = "4MJ",   },
-  -- {name = "wattevilleite",   type = "coal",   fuel_value = "5MJ",   },
-  -- {name = "azurite",         type = "copper",  },
-  -- {name = "bornite",         type = "copper",  },
+  {
+    name = "eitelite",
+    type = "coal",
+    fuel_value = "1MJ",
+    start_placement = true,
+    processing_results = {
+      data.raw.item[enabled.sodium] and {type="item", name=enabled.sodium, amount=1, probability=.2416} or nil,
+      data.raw.item[enabled.magnesium] and {type="item", name=enabled.magnesium, amount=1, probability=.1277} or nil,
+      data.raw.item[enabled.carbon] and {type="item", name=enabled.carbon, probability=.1262} or nil,
+      {type="fluid", name="oxygen", amount=1, probability=.5044}
+    }
+  -- },
+  -- -- {name = "humboldtine",     type = "coal",   fuel_value = "1MJ",   , start_placement = true},
+  -- -- {name = "kochsandorite",   type = "coal",   fuel_value = "0.5MJ", },
+  -- -- {name = "lansfordite",     type = "coal",   fuel_value = "0.5MJ", },
+  -- -- {name = "niveolanite",     type = "coal",   fuel_value = "0.5MJ", },
+  -- -- {name = "refikite",        type = "coal",   fuel_value = "4MJ",   },
+  -- -- {name = "wattevilleite",   type = "coal",   fuel_value = "5MJ",   },
+  -- {
+  --   name = "azurite",
+  --   type = "copper",
+  --   fuel_value = "0.05MJ",
+  --   processing_results = {
+  --     {type="item", name="copper-ore", amount=1, probability=.5531},
+  --     {type="fluid", name="hydrogen", amount=1, probability=.0058},
+  --     data.raw.item[enabled.carbon] and {type="item", name=enabled.carbon, amount=1, probability=.0697},
+  --     {type="fluid", name="oxygen", amount=1, probability=.3714},
+  --   }
+  -- },
+  -- {
+  --   name = "bornite",
+  --   type = "copper",
+  --   start_placement = true,
+  --   processing_results ={
+  --     {type="item", name="iron-ore", amount=1, probability=.1113},
+  --     {type="item", name="copper-ore", amount=1, probability=.6331},
+  --     {type="item", name="sulfur", amount=1, probability=.2556},
+  --   }
+
+  },
   -- {name = "chalcocite",      type = "copper",  },
   -- {name = "chalcopyrite",    type = "copper",  },
   -- {name = "chrysocolla",     type = "copper",  },
@@ -232,7 +277,19 @@ for _, ore in pairs(orelist) do
         pick_sound = item_sounds.wire_inventory_pickup,
         drop_sound = item_sounds.wire_inventory_move,
       },
-      {--add recipe 20 ore to 1 plate or some shit
+      {--Smelting
+        type = "recipe",
+        name = ore.name .. "-smelting",
+        icon = ore.icon .. ".png",
+        icon_size = 64,
+
+        category = "smelting",
+        enabled = true,
+        energy_required = 1,
+        ingredients = {{type="item", name=ore.name, amount=20}},
+        results = {{type="item", name=ore.ore_type, amount=1}}, --Gets the ore type for smelting
+      },
+      {--Processing
         type = "recipe",
         name = ore.name .. "-processing",
         icon = ore.icon .. ".png",
@@ -244,5 +301,6 @@ for _, ore in pairs(orelist) do
         ingredients = {{type="item", name=ore.name, amount=1}},
         results = ore.processing_results
       }
+
     })
 end
